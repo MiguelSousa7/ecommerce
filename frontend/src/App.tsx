28a02a91b9b27897d3a46bf80e8b4b6152115ba4
Miguel Sousa <miguelsousabrg@gmail.com>
2026-09-23 18:19:02 +0100

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+        <button type='button' className='btn btn-outline-primary'>Primary</button>      
+    </div>
+  )
+}
+
+export default App
